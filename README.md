663313adb2f9900d4d0823b9ceedebc034a00257
Gabriel Blanco <gabriel.blancogarcia98@gmail.com>
2022-02-10 21:01:34 +0100

    Add first script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scripts
+
+This repo contains small scripts I create to automate tasks in my PC and make
+my life easier in general. Nothing too crazy, just for fun.
+Contributions are more than welcome!
---
+++ open-book.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Small script to open zathura on a specific book from the dmenu.
+# I originally though about this in a script that would let you choose
+# the folder in which to search, but because I always have my books on
+# ~/books, I decided to keep it simple. This can be maped to a shortcut,
+# which in my case is mod + b.
+
+# Select file to open
+book=$(ls ~/books | dmenu -i -p "Choose book:")
+
+# Send it to zathura and leave it runing with no terminal
+nohup zathura ~/books/$book & exit
